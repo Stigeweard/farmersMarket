867b41bd610ab7bcd42efe779ebbbbd88477b9e2
C S Harding <charliesharding@gmail.com>
2017-06-27 13:45:47 -0500

    refactored register

--- register.py
+++ register.py
@@ -1,104 +1,89 @@
-class Product(object):
-    """Product for sale at the Farmer's Market."""
-    def __init__(self, code, name, price):
-        super(Product, self).__init__()
-        self.code = code
-        self.name = name
-        self.price = price
+# Refactored register
+# this time I tried using some of the style principles I found on https://www.python.org/dev/peps/pep-0008/
 
-# instantiating and naming product variables by their code
-CH1 = Product('CH1', 'Chai', 3.11)
-AP1 = Product('AP1', 'Apples', 6.00)
-CF1 = Product('CF1', 'Coffee', 11.23)
-MK1 = Product('MK1', 'Milk', 4.75)
-OM1 = Product('OM1', 'Oatmeal', 3.69)
+# instead of the class approach, I wanted to try isolating more logic in functions and add a promotion
+# that corresponds to every item, NA being no special price that isn't printed. This way the output will look
+# more like the example and the logic should be easier to follow.
+# Downside is now there is no 'generic' way of defining new functions - however in the last version it constrained
+# what a promotion could offer (i.e: no umbrella discount or percentage discount on all items)
 
-class Promotion(object):
-    """Defining promotion logic."""
-    def __init__(self, name, product, productQuantity, benefitProduct, benefitQuantity, priceReduction, limit=float('inf')):
-        super(Promotion, self).__init__()
-        self.name = name
-        self.product = product
-        self.productQuantity = productQuantity
-        self.benefitProduct = benefitProduct
-        self.benefitQuantity = benefitQuantity
-        self.priceReduction = priceReduction
-        self.limit = limit
+_prices = {'CH1': 3.11, 'AP1': 6.00, 'CF1': 11.23, 'MK1': 4.75, 'OM1': 3.69}
+_promotions = {'BOGO': -11.23, 'APPL': -1.50, 'CHMK': -4.75, 'NA': 0}
 
-# instantiating and naming promotions by their code
-BOGO = Promotion('BOGO', CF1, 1, CF1, 1, CF1.price)
-# 0 for quantity if it should be applied to all existing items
-APPL = Promotion('APPL', AP1, 3, AP1, float('inf'), 1.5)
-CHMK = Promotion('CHMK', CH1, 1, MK1, 1, MK1.price, 1)
+# generic for getting code count of either promotions or prices
+def get_code_count(codes, all_codes):
+    code_count = {}
+    for code in all_codes:
+        code_count[code] = 0
+    for code in codes:
+        code_count[code] += 1
+    return code_count
 
-class Register(object):
-    """docstring for Register."""
-    def __init__(self, basketDict={}, promotions=[], balance=0.0):
-        super(Register, self).__init__()
-        self.basketDict = basketDict
-        self.balance = balance
-        self.promotions = promotions
-        self.promoItems = []
-        self.affectedItems = []
-        for promo in promotions:
-            self.promoItems.append(promo.product.code)
+# the following 3 functions are called on final basket items and return appropriate promo code to apply or 'NA'
+# checks if at least 3 apples
+def check_appl(basket):
+    appl_count = get_code_count(basket, _prices.keys())
+    if appl_count['AP1'] > 2:
+        return 'APPL'
+    else:
+        return 'NA'
 
-    def applyPromotion(self, promotion):
-        self.promotions.append(promotion)
-        self.promoItems.append(promotion.product.code)
-        print 'Applied new promotion: ' + promotion.name
+# checks if more than one coffee has been bought, adds promo on even occurences
+def check_bogo(basket, promos):
+    code_count = get_code_count(basket, _prices.keys())
+    promo_count = get_code_count(promos, _promotions.keys())
+    if code_count['CF1']/2 > promo_count['BOGO']:
+        return 'BOGO'
+    else:
+        return 'NA'
 
-    def addItem(self, item, quantity=1):
-        # if item already exists in basket, add to quantity. otherwise add to basket with given quantity
-        if item.code in self.basketDict:
-            self.basketDict[item.code]['quantity'] += quantity
-        else:
-            self.basketDict[item.code] = {}
-            self.basketDict[item.code]['quantity'] = quantity
-            self.basketDict[item.code]['itemObj'] = item
+# checks for milk promo if chai has been bought, only returns CHMK if it isn't in promo count (limit 1)
+def check_chmk(basket, promos):
+    code_count = get_code_count(basket, _prices.keys())
+    promo_count = get_code_count(basket, _promotions.keys())
+    if code_count['CH1'] > 0 and promo_count['CHMK'] == 0:
+        return 'CHMK'
+    else:
+        return 'NA'
 
-    def total(self):
-        self.balance = 0
-        print '{:20} {:^20} {:>20}'.format('item', 'promotion', 'price')
-        print '{:20} {:^20} {:>20}'.format('----', '---------', '-----')
-        for item in self.basketDict:
-            for promo in self.promotions:
-                if promo.product.name == item and promo.limit > 0 and promo.benefitQuantity > 0 and self.basketDict[item]['quantity'] >= promo.productQuantity:
-                    # this needs to append as many items as allows considering limit, benefitQuantity, and productQuantity
-                    self.affectedItems.append(item)
-                    promo.limit -= 1
-                    promo.benefitQuantity -= 1
+# sums up everything in basket and promos
+def calculate_final_sum(basket, promos):
+    total = 0
+    for code in basket:
+        total += _prices[code]
+    for code in promos:
+        total += _promotions[code]
+    return total
 
+# neat output string formatting to correct decimal places, using idx as the index corresponding to item position
+# so that the applicable promo will print out below the item it applies to
+def print_register(basket, promo, final_sum):
+    print("{l: <10}{r: >25}".format(l="Item", r="Price"))
+    print("{l: <10}{r: >25}".format(l="----", r="-----"))
+    for idx, item in enumerate(basket):
+        # print item and price
+        print("{l: <10}{r: >25.2f}".format(l=item, r=_prices[item]))
+        if promo[idx] is not 'NA': # don't bother to print an NA promotion
+            # print applicable promotion
+            print("{l: >15}{r: >20.2f}".format(l=promo[idx], r=_promotions[promo[idx]]))
+    print('{:-^35}'.format('-'))
+    print("{r: >35.2f}".format(r=final_sum))
 
+# initialize basket and promo as lists so that order is retained and index can be used in function above
+_basket = []
+_basket.append('AP1')
+_basket.append('AP1')
+_basket.append('CH1')
+_basket.append('AP1')
+_promo = []
 
-        print '--------------------------------------------------------------'
-        print 'Current total: $' + str(self.balance)
-        # # loop over items in basket
-        # for item in self.basketDict:
-        #     timesToPrint = self.basketDict[item]['quantity']
-        #     while timesToPrint > 0:
-        #         print '{:20} {:>40}'.format(item, str(self.basketDict[item]['itemObj'].price))
-        #         timesToPrint -= 1
-        #     # check if current item exists in affectedItems
-        #     for promo in self.promotions:
-        #         # see if current item in basket has a possible applicable promotion
-        #         if item == promo.product.code:
-        #             # check to see if promotion requirements are met
-        #             if promo.limit > 0:
-        #                 # add affected item to list of affected items
-        #                 print promo.productQuantity
-        #                 if self.basketDict[item]['quantity'] >= promo.productQuantity:
-        #                     self.affectedItems.append(item)
-        #                     promo.limit -= 1
-        #             else:
-        #                 pass
-        #     if item in self.affectedItems:
-        #         print 'lol'
-        #     self.balance += self.basketDict[item]['itemObj'].price * self.basketDict[item]['quantity']
-
-test = Register()
-test.applyPromotion(BOGO)
-test.addItem(OM1)
-test.addItem(CF1)
-test.addItem(CF1)
-test.total()
+for item in _basket:
+    if item is 'AP1':
+        _promo.append(check_appl(_basket))
+    elif item is 'CF1':
+        _promo.append(check_bogo(_basket, _promo))
+    elif item is 'MK1':
+        _promo.append(check_chmk(_basket, _promo))
+    else:
+        _promo.append('NA')
+print_register(_basket, _promo, calculate_final_sum(_basket, _promo))
